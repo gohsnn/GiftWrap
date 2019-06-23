2a7d304b1f8ad5a2cd8a8e11efe3728d6b5005c4
Arthur Lee <ykarthurlee@gmail.com>
2019-06-24 02:13:44 +0800

    revert to first change

--- android/app/src/main/java/com/giftwrap/MainApplication.java
+++ android/app/src/main/java/com/giftwrap/MainApplication.java
@@ -2,14 +2,7 @@ package com.giftwrap;
 
 import android.app.Application;
 
-import com.facebook.FacebookSdk;
-import com.facebook.CallbackManager;
-import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
-import io.invertase.firebase.RNFirebasePackage;
-import io.invertase.firebase.database.RNFirebaseDatabasePackage;
-import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -20,12 +13,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
-  
-  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
-
-  protected static CallbackManager getCallbackManager() {
-    return mCallbackManager;
-  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -37,10 +24,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FBSDKPackage(mCallbackManager),
-            new RNFirebasePackage(),
-            new RNFirebaseDatabasePackage(),
-            new RNFirebaseAuthPackage(),
             new RNGestureHandlerPackage()
       );
     }
